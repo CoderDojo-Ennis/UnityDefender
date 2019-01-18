5407a5d724cf78d7b9925691d1958b20d8bde7b3
Russ Painter <russp@geekymonkey.com>
2019-01-18 22:54:14 +0000

    ufo hover & spin

---
+++ Assets/UfoHoverScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UfoHoverScript : MonoBehaviour
+{
+    public float HoverHeight = 1;
+    public float HoverSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float y = Mathf.Sin(Time.time * HoverSpeed) * HoverHeight;
+        transform.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
+    }
+}
---
+++ Assets/UfoSpinScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UfoSpinScript : MonoBehaviour
+{
+    public float SpinSpeed = 360;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, SpinSpeed * Time.deltaTime, 0);
+    }
+}
