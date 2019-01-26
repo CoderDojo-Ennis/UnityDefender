78437300f9c19fe550cc039cb3c3cd6caf5bd29f
Russ Painter <russp@geekymonkey.com>
2019-01-26 09:30:05 +0000

    Player movement cotrols

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float ForwardSpeed = 30;
+    public float PitchSpeed = -2;
+    public float YawSpeed = 2;
+    public Vector2 InputXY;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = GetInput();
+
+        Vector3 forward = this.transform.forward * ForwardSpeed * Time.deltaTime;
+        Vector3 pitch = this.transform.up * input.y * PitchSpeed * Time.deltaTime;
+        Vector3 yaw = this.transform.right * input.x * YawSpeed * Time.deltaTime;
+        Vector3 direction = forward + pitch + yaw;
+        this.transform.rotation = Quaternion.LookRotation(direction);
+
+        this.transform.Translate(Vector3.forward * ForwardSpeed * Time.deltaTime);
+    }
+
+    private Vector2 GetInput()
+    {
+        Vector2 input;
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+        return input;
+    }
+}
---
+++ Assets/PlayerRoll.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRoll : MonoBehaviour
+{
+    public float RollAmount = 30;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float roll = Input.GetAxis("Horizontal") * RollAmount;
+        transform.localEulerAngles = new Vector3(0, 180, roll);
+    }
+}
