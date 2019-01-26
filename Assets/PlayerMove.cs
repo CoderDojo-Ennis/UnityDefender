f4386f1bb424b4c008c2cd870b837b3a550d4142
Russ Painter <russp@geekymonkey.com>
2019-01-26 11:01:26 +0000

    Add comments and remove collisions

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -14,12 +14,20 @@ public class PlayerMove : MonoBehaviour
     {
         Vector2 input = GetInput();
 
+        // Calculate how much forward to move this frame
         Vector3 forward = this.transform.forward * ForwardSpeed * Time.deltaTime;
+
+        // Calulate how much up and to the right to move this frame
         Vector3 pitch = this.transform.up * input.y * PitchSpeed * Time.deltaTime;
         Vector3 yaw = this.transform.right * input.x * YawSpeed * Time.deltaTime;
+
+        // The total movement is just adding these vectors
         Vector3 direction = forward + pitch + yaw;
+
+        // Convert this to a rotation and rotate our ship
         this.transform.rotation = Quaternion.LookRotation(direction);
 
+        // Move the ship forward in it's new direction
         this.transform.Translate(Vector3.forward * ForwardSpeed * Time.deltaTime);
     }
 
