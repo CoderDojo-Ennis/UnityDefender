8ba39227123c344cda7e7068c8a3595ebe59aba0
Russ Painter <russp@geekymonkey.com>
2019-01-26 21:13:29 +0000

    Shooting

---
+++ Assets/LaserLifespan.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserLifespan : MonoBehaviour
+{
+    public float LifeSpan = 0.2f;
+    private float Age = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Age += Time.deltaTime;
+        if (Age >= LifeSpan)
+        {
+            Destroy(gameObject);
+        } else
+        {
+            // Scale down the X, and Y, but keep the Z scale value the same
+            float scale = 1 - (Age / LifeSpan);
+            transform.localScale = new Vector3(scale, scale, transform.localScale.z);
+        }
+    }
+}
---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject LaserPrefab;
+    public AudioSource LaserSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LaserSound = GetComponent<AudioSource>();           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Create a new laser at the position and rotation of the gun
+            Instantiate(LaserPrefab, transform.position, transform.rotation);
+
+            LaserSound.Play();
+        }   
+    }
+}
