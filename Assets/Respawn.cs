177fe6988a680e9cf5270250502994fb28b8f651
Russ Painter <russp@geekymonkey.com>
2019-02-09 08:31:38 +0000

    Respawn

---
+++ Assets/Respawn.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public GameObject RespawnPrefab;
+    public Vector3 RespawnPosition;
+    public float DelaySeconds = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("RespawnNow", DelaySeconds);   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void RespawnNow()
+    {
+        if (RespawnPrefab != null)
+        {
+            if (RespawnPosition != null)
+            {
+                Instantiate(RespawnPrefab, RespawnPosition, Quaternion.identity);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
