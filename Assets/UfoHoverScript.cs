4ffacc524ec97ffa3ce198bb86cca4dfc37b0b30
Russ Painter <russp@geekymonkey.com>
2019-02-09 07:39:15 +0000

    Ufo Randomness

--- Assets/UfoHoverScript.cs
+++ Assets/UfoHoverScript.cs
@@ -6,17 +6,20 @@ public class UfoHoverScript : MonoBehaviour
 {
     public float HoverHeight = 1;
     public float HoverSpeed = 1;
+    private float TimeOffset;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        TimeOffset = Random.Range(0.0f, 5.0f);
+        HoverHeight *= Random.Range(.9f, 1.1f);
+        HoverSpeed *= Random.Range(0.9f, 1.1f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float y = Mathf.Sin(Time.time * HoverSpeed) * HoverHeight;
+        float y = Mathf.Sin((Time.time + TimeOffset) * HoverSpeed) * HoverHeight;
         transform.localPosition = new Vector3(transform.localPosition.x, y, transform.localPosition.z);
     }
 }
--- Assets/UfoSpinScript.cs
+++ Assets/UfoSpinScript.cs
@@ -6,6 +6,11 @@ public class UfoSpinScript : MonoBehaviour
 {
     public float SpinSpeed = 360;
 
+    private void Start()
+    {
+        SpinSpeed *= Random.Range(0.9f, 1.1f);
+    }
+
     // Update is called once per frame
     void Update()
     {
