d9d60bfa1b45ad86fe839ed126b10264305caca6
Russ Painter <russp@geekymonkey.com>
2019-04-06 06:47:02 +0100

    Cleanup

--- Assets/Die.cs
+++ Assets/Die.cs
@@ -6,18 +6,6 @@ public class Die : MonoBehaviour
 {
     public GameObject ChunksPrefab;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void DieNow()
     {
         Destroy(gameObject);
